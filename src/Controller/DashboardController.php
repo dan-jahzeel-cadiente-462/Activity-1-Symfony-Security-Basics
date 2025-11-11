44bbeb077c553194cf5b615714b93d05a3f243f6
Dan Jahzeel Cadiente <dcadiente462@gmail.com>
2025-11-11 21:09:22 +0800

    Descriptive commit message for my changes

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,17 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
+    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_USER');
+        // Access is enforced by the #[IsGranted] attribute. If the current user
+        // is not granted ROLE_USER an AccessDeniedException will be thrown and
+        // handled by Symfony (rendering a 403 page or redirect depending on
+        // your security configuration).
 
-        return $this->render('dashboard/index.html.twig', [
-            'message' => 'Welcome to Dashboard',
-            'user' => $this->getUser(),
-        ]);
+        // Render the dashboard template which shows role-based messages.
+        return $this->render('dashboard/index.html.twig');
     }
 }
